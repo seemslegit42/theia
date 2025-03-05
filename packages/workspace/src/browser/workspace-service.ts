57016e66d082cd330939ab6eea571a2d246a74be
Jonah Iden <jonah.iden@typefox.io>
2025-03-05 13:11:21 +0100

    use contribution instead of rebind
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/workspace/src/browser/workspace-service.ts
+++ packages/workspace/src/browser/workspace-service.ts
@@ -44,9 +44,9 @@ export interface WorkspaceOpenHandlerContribution {
     getWorkspaceLabel?(uri: URI): MaybePromise<string | undefined>;
 }
 
+export const WorkspaceHandlingContribution = Symbol('WorkspaceHandlingContribution');
 export interface WorkspaceHandlingContribution {
     modifyRecentWorksapces?(workspaces: string[]): MaybePromise<string[]>;
-
 }
 
 /**
@@ -107,6 +107,9 @@ export class WorkspaceService implements FrontendApplicationContribution, Worksp
 
     @inject(ContributionProvider) @named(WorkspaceOpenHandlerContribution)
     protected readonly openHandlerContribution: ContributionProvider<WorkspaceOpenHandlerContribution>;
+
+    @inject(ContributionProvider) @named(WorkspaceHandlingContribution)
+    protected readonly workspaceHandlingContribution: ContributionProvider<WorkspaceHandlingContribution>;
 
     protected _ready = new Deferred<void>();
     get ready(): Promise<void> {
@@ -336,7 +339,15 @@ export class WorkspaceService implements FrontendApplicationContribution, Worksp
     }
 
     async recentWorkspaces(): Promise<string[]> {
-        return this.server.getRecentWorkspaces();
+        let recentWorkspaces = await this.server.getRecentWorkspaces();
+
+        for (const handler of this.workspaceHandlingContribution.getContributions()) {
+            if (handler.modifyRecentWorksapces) {
+                recentWorkspaces = await handler.modifyRecentWorksapces(recentWorkspaces);
+            }
+        }
+
+        return recentWorkspaces;
     }
 
     async removeRecentWorkspace(uri: string): Promise<void> {
